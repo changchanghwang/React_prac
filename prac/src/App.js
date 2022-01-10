20a6c61f16095acc0970a999b9f8fc5c0b073e66
CH_Hwang <hch950627@naver.com>
2022-01-10 23:34:02 +0900

    [feat] 스타일드 컴포넌트

--- prac/src/App.js
+++ prac/src/App.js
@@ -5,13 +5,33 @@ import styled from 'styled-components';
 function App() {
   return (
     <div id="id" className="App">
-      <div className="container">
-        <h1 className="title">내 버킷리스트</h1>
-        <hr className="line" />
+      <Container>
+        <Title>내 버킷리스트</Title>
+        <Line />
         <BucketList list={['영화관 가기', '매일 책읽기', '수영 배우기']} />
-      </div>
+      </Container>
     </div>
   );
 }
+
+const Container = styled.div`
+  background-color: #fff;
+  width: 50vw;
+  max-width: 350px;
+  margin: auto;
+  height: 80vh;
+  padding: 16px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+`;
+
+const Title = styled.h1`
+  color: slateblue;
+  text-align: center;
+`;
+
+const Line = styled.hr`
+  margin: 16px 0px;
+`;
 
 export default App;
