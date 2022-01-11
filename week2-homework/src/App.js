ecdff49f5f23f7ad5b78c0a8be1b88309868c266
CH_Hwang <hch950627@naver.com>
2022-01-11 22:00:37 +0900

    [feat] 2주차 숙제

--- week2-homework/src/App.js
+++ week2-homework/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
 
 function App() {
+  const n = '황창환';
+  const text = React.createRef();
+  const [name, setName] = React.useState(n);
+  const changeName = () => {
+    const newName = text.current.value;
+    setName(newName);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrap>
+      <div>
+        <Me>
+          나는 <Name>{name}</Name>에 대해서
+        </Me>
+        <Me>얼마나 알고 있을까?</Me>
+        <Me>
+          <input placeholder="내 이름" ref={text} />
+        </Me>
+        <Me>
+          <button onClick={changeName}>시작하기</button>
+        </Me>
+      </div>
+    </AppWrap>
   );
 }
+
+const AppWrap = styled.div`
+  border: 1px solid black;
+  width: 300px;
+  height: 50rem;
+  margin: auto;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Me = styled.div`
+  font-size: 20px;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 10px;
+`;
+
+const Name = styled.div`
+  background-color: yellow;
+  border: 1px solid black;
+  border-radius: 20%;
+  text-align: center;
+  margin: 0px 20px;
+`;
 
 export default App;
